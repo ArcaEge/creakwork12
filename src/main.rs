48a08fa6b5f603858e6ef56752b7ed373bf8a5ef
Arca Cengiz <arcaegecengiz@gmail.com>
2026-07-29 15:34:44 +0100

    audio playback works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use rodio::{Decoder, DeviceSinkBuilder, Player, Source};
+use std::io::Cursor;
+use std::thread::sleep;
+
 fn main() {
-    println!("Hello, world!");
+    let mut handle = DeviceSinkBuilder::open_default_sink().expect("open default audio sink");
+    handle.log_on_drop(false);
+
+    let player = Player::connect_new(&handle.mixer());
+
+    let creak_file = include_bytes!("../assets/creak.wav");
+    let creak_cursor = Cursor::new(creak_file);
+    let creak = Decoder::builder()
+        .with_data(creak_cursor)
+        .with_hint("wav")
+        .with_gapless(true)
+        .build()
+        .expect("decode creak")
+        .repeat_infinite();
+
+    player.append(creak);
+    // player.pause();
+
+    sleep(std::time::Duration::from_millis(5000));
+    player.pause();
+    sleep(std::time::Duration::from_millis(500));
+    player.play();
+    sleep(std::time::Duration::from_millis(5000));
 }
